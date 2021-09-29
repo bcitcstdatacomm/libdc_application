b34ec257fc056b6973f673e9d5e43e5a8582b96c
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2021-09-29 10:21:01 -0700

    fixed includes

--- src/application.c
+++ src/application.c
@@ -15,10 +15,10 @@
  */
 
 #include "application.h"
-#include <dc_application/command_line.h>
-#include <dc_application/config.h>
-#include <dc_application/defaults.h>
-#include <dc_application/environment.h>
+#include "dc_application/command_line.h"
+#include "dc_application/config.h"
+#include "dc_application/defaults.h"
+#include "dc_application/environment.h"
 #include <dc_fsm/fsm.h>
 #include <dc_posix/dc_stdlib.h>
 #include <dc_posix/dc_string.h>
